698cd9a490bbba9719413c1e368f1916a8b64cfe
Wilson Huallpa <wilsonhuallpapro@gmai.com>
2021-11-30 21:39:07 -0300

    pruba dos

--- app/models/Mesa.php
+++ app/models/Mesa.php
@@ -26,7 +26,7 @@ class Mesa
         return $consulta->fetchAll(PDO::FETCH_CLASS, 'Mesa');
     }
 
-    public static function obtenerMesa($codigo)
+    public static function obenerMesa($codigo)
     {
         $objAccesoDatos = AccesoDatos::obtenerInstancia();
         $consulta = $objAccesoDatos->prepararConsulta("SELECT id, codigo, id_estado FROM mesas WHERE codigo = :codigo");
